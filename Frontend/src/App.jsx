2f71878886c705838f1560250042c2b4c8b921fe
Atul Tiwari <atultiwari2428@gmail.com>
2026-09-04 23:03:24 +0530

    Again

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,7 +1,7 @@
 import { Routes, Route, Navigate } from "react-router";
 import Homepage from "./pages/Homepage";
 import Login from "./pages/Login";
-import Signup from "./pages/signup";
+import Signup from "./pages/Signup";
 import { checkAuth } from "./authSlice";
 import { useDispatch, useSelector } from "react-redux";
 import { useEffect } from "react";
--- Frontend/src/utils/axiosClient.js
+++ Frontend/src/utils/axiosClient.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosClient = axios.create({
-    baseURL: "http://localhost:3000",
+    baseURL: "https://online-coding-platform-1.onrender.com",
     withCredentials: true,
     headers: {
         "Content-Type": "application/json"
@@ -9,5 +9,6 @@ const axiosClient = axios.create({
 });
 
 export default axiosClient;
+
 
 
