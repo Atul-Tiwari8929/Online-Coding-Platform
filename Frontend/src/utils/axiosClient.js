9c4bb5128c958bca542c44bde024ad33e02d5e5f
Atul Tiwari <atultiwari2428@gmail.com>
2026-09-04 23:16:58 +0530

    newDomain

--- Frontend/src/utils/axiosClient.js
+++ Frontend/src/utils/axiosClient.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosClient = axios.create({
-    baseURL: "https://online-coding-platform-1.onrender.com",
+    baseURL: "https://online-coding-platform-2.onrender.com",
     withCredentials: true,
     headers: {
         "Content-Type": "application/json"
