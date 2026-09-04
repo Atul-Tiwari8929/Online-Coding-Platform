5579076dfac5aabf4e51f831162955a38e5ae7a2
Atul Tiwari <atultiwari2428@gmail.com>
2026-09-05 00:42:47 +0530

    FixedCorsCommit

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -13,7 +13,7 @@ const aiRouter = require("./routes/aiChatting")
 const cors = require('cors')
 
 app.use(cors({
- origin: 'http://localhost:5173',
+ origin: 'https://online-coding-platform-1n6d.vercel.app',
  credentials:true
 
 }))  
